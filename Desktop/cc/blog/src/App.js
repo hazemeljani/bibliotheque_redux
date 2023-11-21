2fac3d35576ba1f817d0fe20b85d16397f20b0b0
hazem <hazemjh10@gmail.com>
2023-11-21 13:09:52 +0100

    final

---
+++ Desktop/cc/blog/src/App.js
@@ -0,0 +1,13 @@
+import Book from "./component/book";
+import Pen from "./component/pen";
+
+function App() {
+  return (
+    <div className="App">
+      <Book></Book>
+      <Pen></Pen>
+    </div>
+  );
+}
+
+export default App;
